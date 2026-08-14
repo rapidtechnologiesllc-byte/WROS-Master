efc81eb5e4b2dba6a8093dd82642c2e500f4d85e
Avinash Mukundan <am@rapidtechservices.com>
2026-08-14 18:27:27 -0400

    Add postcss-calc for Ant Design v6 CSS calc() support

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+  plugins: [
+    require('postcss-calc'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 };
